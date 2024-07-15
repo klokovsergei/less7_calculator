cce0d66c3be47f5205266ddc44e6c934f7e48739
Sergei Klokov <s2584835@ya.ru>
2024-07-15 13:44:26 +0300

    не успеваю сделать через MAP, пока сдаю заготовку

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,8 @@
 package less7_calculator;
 
 import less7_calculator.model.ComplexNumber;
-import less7_calculator.service.Calculable;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class Calculator {
-    private final static Map<String, Calculable> operations = new HashMap<>();
     public static void main(String[] args) {
         ComplexNumber number = new ComplexNumber(-1.0, -8D);
         System.out.println(number);
--- service/CalculatorService.java
+++ service/CalculatorService.java
@@ -2,8 +2,37 @@ package less7_calculator.service;
 
 import less7_calculator.model.ComplexNumber;
 
-public class CalculatorService {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class CalculatorService {
+    private final static Map<String, Calculable> operations = new HashMap<>();
+    static {
+        operations.put("+", args -> {
+            double[] x = args[0].getComplexNumber();
+            double[] y = args[1].getComplexNumber();
+            return new ComplexNumber(x[0] + y[0], x[1] + y[1]);
+        });
+        operations.put("-", args -> new ComplexNumber(0D,0D));
+        operations.put("/", args -> new ComplexNumber(0D,0D));
+    }
+
+    private static double calculate() {
+        double operand1 = Double.parseDouble(prompt("Enter first number: "));
+        String operator = prompt("Enter math operation (+ - /): ");
+        double operand2 = Double.parseDouble(prompt("Enter second number: "));
+
+
+        if (operations.containsKey(operator))
+            return operations.get(operator).calculate(operand1, operand2);
+        else
+            throw new RuntimeException("Операция не реализована или не поддерживается");
+    }
+    private static String prompt(String message) {
+        System.out.println(message);
+        return new Scanner(System.in).next();
+    }
 }
 interface Calculable{
     ComplexNumber calculate(ComplexNumber ...args);
